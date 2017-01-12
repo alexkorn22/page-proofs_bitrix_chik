7ef0824d1452754e86f4bf95d0f7566d404f7605
alex_korn_22 <kornsasha89@gmail.com>
2017-01-12 12:04:31 +0200

    Готова страница брендов

---
+++ www/brands.php
@@ -0,0 +1,75 @@
+<? include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
+
+<div class="container">
+
+    <!--ПУТЬ -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="path">
+                <span><a href="#">Главная</a> </span>
+                <i class="fa fa-angle-right"></i>
+                <span> <a href="#">Бренды</a> </span>
+            </div>
+        </div>
+    </div>
+    <!--Заголовок страницы -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="page_title">
+                <hr>
+                <span>Бренды</span>
+                <hr>
+            </div>
+        </div>
+    </div>
+    <!--Списки брендов -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="block_list_brands">
+                <div class="row">
+                    <div class="col-md-2">
+                        <ul class="list-unstyled list_brand">
+                            <span>L</span>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                        </ul>
+                        <ul class="list-unstyled list_brand">
+                            <span>L</span>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <ul class="list-unstyled list_brand">
+                            <span>L</span>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                            <li><a href="#">Название</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-2"></div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<? include($_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
--- www/personal.php
+++
@@ -1,7 +0,0 @@
-<? include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
-
-<div class="container">
-
-</div>
-
-<? include($_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
