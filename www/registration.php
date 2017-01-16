7eef76771d23bbde737a1dbe941b1ac906c4da8e
alex_korn_22 <kornsasha89@gmail.com>
2017-01-16 10:40:08 +0200

    Структура для страницы регистрации

---
+++ www/registration.php
@@ -0,0 +1,9 @@
+<? include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
+
+    <div class="container">
+        <div id="registration">
+
+        </div>
+    </div>
+
+<? include($_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
